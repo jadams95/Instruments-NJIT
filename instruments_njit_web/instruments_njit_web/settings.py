e569e617d7e83709163d465b0e23c62c98dec6fb
John Adams <adamsjt95@gmail.com>
2023-12-02 15:05:09 -0500

    Finished Templates

--- instruments_njit_web/instruments_njit_web/settings.py
+++ instruments_njit_web/instruments_njit_web/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Adam1212DB',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
