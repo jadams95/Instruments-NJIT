374ad378fc4f5af65255cc69f735c23e1b8a4458
John Adams <adamsjt95@gmail.com>
2023-12-02 15:04:18 -0500

    Templates Finished Setup

--- instruments_njit_web/instruments_njit_web/settings.py
+++ instruments_njit_web/instruments_njit_web/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'postgres',
+        'PASSWORD': 'Adam1212DB',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
