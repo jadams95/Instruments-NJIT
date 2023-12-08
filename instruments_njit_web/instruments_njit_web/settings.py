4dab9cfe9b9bdc171d84461ddf5670563df1fd6e
kk682 <kk682@njit.edu>
2023-12-07 19:31:11 -0500

    Changes

--- instruments_njit_web/instruments_njit_web/settings.py
+++ instruments_njit_web/instruments_njit_web/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'postgres',
+        'PASSWORD': 'vk1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
